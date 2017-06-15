529572c0185762502d2e37bd772065cb397688dd
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-06-15 15:05:48 +0200

    Modification des fichier app et controllers
    Modification des fichier app et controllers.php

--- Saraba/src/app.php
+++ Saraba/src/app.php
@@ -1,20 +1,67 @@
 <?php
 
+use Repository\ArticleRepository;
+use Repository\CategoryRepository;
+use Repository\UserRepository;
+use Service\UserManager;
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
-$app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
+$app['twig'] = $app->extend('twig', function (Twig_Environment $twig, Application $app) {
+    
+// pour avoir accès au service UserManager dans les templates
+    $twig->addGlobal('user_manager', $app['user.manager']);
 
     return $twig;
 });
 
+$app->register(
+    new DoctrineServiceProvider(),
+    [
+        'db.options' => [
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'silex_blog',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            
+        ]
+    ]
+);
+
+$app->register(new SessionServiceProvider());
+$app->register(new ValidatorServiceProvider());
+
+
+/* Repository */
+
+$app['category.repository'] = function () use ($app){
+    return new CategoryRepository($app['db']);
+};
+
+$app['article.repository'] = function () use ($app){
+    return new ArticleRepository($app['db']);
+};
+
+$app['user.repository'] = function () use ($app){
+    return new UserRepository($app['db']);
+};
+
+/* Services */
+
+$app['user.manager'] = function () use ($app){
+    return new UserManager($app['session']);
+};
 return $app;
--- Saraba/src/controllers.php
+++ Saraba/src/controllers.php
@@ -1,20 +1,117 @@
 <?php
 
+use Controller\IndexController;
+use Controller\UserController;
+use Controller\Admin\ArticleController;
+use Controller\Admin\CategoryController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
-;
+$app['index.controller'] = function () use ($app) {
+    return new IndexController($app);
+};
 
-$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+$app
+    ->get('/', 'index.controller:indexAction')  
+    ->bind('homepage');
+
+$app
+    ->get('/rubriques', 'index.controller:categoriesAction')  
+    ->bind('categories');
+
+$app['admin.category.controller'] = function () use ($app) {
+    return new CategoryController($app);
+};
+
+$app
+    ->get('/rubriques/{id}', 'index.controller:categorieAction')
+    ->assert('id', '\d+')
+    ->bind('category');
+
+/* Utilisateur */
+
+$app['user.controller'] = function () use ($app) {
+    return new UserController($app);
+};
+
+$app
+    ->match('utilisateur/inscription', 'user.controller:registerAction')
+    ->bind ('register');
+
+$app
+    ->match('utilisateur/connexion', 'user.controller:loginAction')
+    ->bind ('login');
+
+$app
+    ->get('utilisateur/deconnexion', 'user.controller:logoutAction')
+    ->bind ('logout');
+
+/* Admin */
+
+$app['admin.category.controller'] = function()use($app){
+  return new CategoryController($app);
+};
+// Créer un sous-ensemble de routes
+$admin = $app['controllers_factory'];
+
+// permet de faire un traitement avant l'accès à la route
+$admin->before(function() use ($app){ 
+    if(!$app['user.manager']->isAdmin()){ // si un admin n'est pas connecté
+        $app->abort(403, 'Accès refusé'); // HTTP 403 Forbidden
+    }
+});
+
+
+// Créer un sous-ensemble de routes
+$app->mount('/admin', $admin);
+
+$admin
+    ->get('/rubriques', 'admin.category.controller:listAction')  
+    ->bind('admin_categories');
+
+$admin
+    ->match('/rubriques/edition/{id}', 'admin.category.controller:editAction') //match accepte plusieurs méthodes, nomtamment get et post
+    ->value('id', null) // valeur par défaut (null) pour le paramètre (id) de la route
+    ->bind('admin_category_edit');
+
+$admin
+    ->match('/rubriques/suppression/{id}', 'admin.category.controller:deleteAction') //match accepte plusieurs méthodes, nomtamment get et post
+    ->bind('admin_category_delete');
+
+
+/*
+ * Créer la partie admin pour les articles :
+ * - Créer le contrôleur Admin\ArticleController
+ * - le définir en service
+ * - on y ajoute la méthode listAction à vide
+ * - puis la route qui ponte dessus
+ * - on ajoute le lien vers cette route dans la navbar admin
+ * - on crée l'entity Article et le repository Article Repository
+ * - on remplit la méthode listAction du contrôleur en utilisant ArticleRepository
+ * - on crée la vue qui affiche les articles dans un tableau html
+ */
+
+$app['admin.article.controller'] = function () use ($app) {
+    return new ArticleController($app);
+};
+
+$admin
+    ->get('/articles', 'admin.article.controller:listAction')  
+    ->bind('admin_articles');
+
+$admin
+    ->match('/articles/edition/{id}', 'admin.article.controller:editAction') //match accepte plusieurs méthodes, nomtamment get et post
+    ->value('id', null) // valeur par défaut (null) pour le paramètre (id) de la route
+    ->assert('id', '\d+')
+    ->bind('admin_article_edit');
+
+$admin
+    ->match('/articles/suppression/{id}', 'admin.article.controller:deleteAction') //match accepte plusieurs méthodes, nomtamment get et post
+    ->bind('admin_article_delete');
+
+$app->error(function (Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
     }

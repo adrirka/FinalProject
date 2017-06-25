6f85f57756ab7f88afaeb2b2ef8bcc78ad392e64
adrirka <rousselet.adrien@gmail.com>
2017-06-25 13:11:12 +0200

    template_hp_update_2506

--- OurOwnTemplate/js/script.js
+++ OurOwnTemplate/js/script.js
@@ -2,33 +2,31 @@
 
 $( document ).ready( function(){
 
-    // ouverture 
+    // BURGER MENU RESPONSIVE 
     $( 'nav p' ).click( function(evt){
 
-        // Bloquer le comportement naturel de la balise A
         evt.preventDefault();
 
-        // Appliquer la fonction slideToggle sur la nav
         $( 'nav ul' ).slideToggle();
 
     });
 
-    // Burger menu : navigation 
+    
     $( 'nav li > a' ).click( function(evt){
 
-        
         evt.preventDefault();
 
        var linkToOpen = $( this ).attr( 'href' );
         
-        // Fermeture
         $( 'nav ul' ).slideUp( function(){
 
-            // Changer de page 
              window.location = linkToOpen;
 
         }); 
             
-    }); 
+    }); // Fin du BURGER MENU
+
+    // PROGRESS BAR ROUNDED
+
 
 }); // Fin de la fonction d'attente du chargement du DOM
